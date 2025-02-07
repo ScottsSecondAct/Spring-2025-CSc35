6fdd5ca3f26156abef0c2eac9b33e3f05dd9886f
Scott Davis <scott@scottssecondact.com>
2025-02-07 10:36:54 -0800

    Added more comments to help readability.

--- week3/compare_numbers.asm
+++ week3/compare_numbers.asm
@@ -1,60 +1,63 @@
 .intel_syntax noprefix
 
 .bss
-	num1:	.space 8
-	num2:	.space 8
+	num1:	.space 8 # Allocate 8 bytes for an integer.
+	num2:	.space 8 # Allocate 8 bytes for an integer.
 
 .data
 Prompt1:
-	.ascii "Enter first number: \0"
+	.ascii "Enter the first number: \0" # Note that there is no "\n".  This is so that the prompt and number will be on the same line in the console.
 
 Prompt2:
-	.ascii "Enter second number: \0"
+	.ascii "Enter the second number: \0" # Note that there is no "\n".  This is so that the prompt and number will be on the same line in the console.
 
 Greater_Response:
-	.ascii "First number is greater\n\0"
+	.ascii "The first number is greater.\n\0"
 
 Less_Response:
-	.ascii "First number is less\n\0"
+	.ascii "The first number is less.\n\0"
 
 Equal_Response:
-	.ascii "Both numbers are equal\n\0"
+	.ascii "Both numbers are equal.\n\0"
 
 .text
-.global Main
+.global Main # Program entry point.
 
 Main:
+  # Prompt for the first number and store it in num1.
 	lea rdx, Prompt1
 	call PrintString
-	call ScanInteger
+	call ScanInteger # Returns the integer in the rdx register.
 	mov [num1], rdx
 
+  # Prompt for the second number and store it in num2.
 	lea rdx, Prompt2
 	call PrintString
-	call ScanInteger
+	call ScanInteger # Returns the integer in the rdx register.
 	mov [num2], rdx
 
-	mov rax, [num1]
-	mov rbx, [num2]
+	mov rax, [num1] # Move num1 into rax.  This is arbitrary and any general purpose register could have been used.
+	mov rbx, [num2] # Move num2 into rbx.  This is arbitrary and any general purpose register could have been used.
 
-	cmp rax, rbx
+	cmp rax, rbx # Compare rax and rbx.
 
-	jl first_lesser	
-	jg first_greater
+	jl first_lesser	# If rax < rbx, then jump to the first_lesser label.
+	jg first_greater 	# If rax > rbx, then jump to the first_greater label.
 
+  # rax is not greater than or less than rbx, so they must be equal.
 	lea rdx, Equal_Response
 	call PrintString
-	jmp exit
+	jmp exit # Jump over first_greater and first_lesser.
 
 first_greater:
 	lea rdx, Greater_Response
 	call PrintString
-	jmp exit
+	jmp exit # Jump ove first_lesser.
 
 first_lesser:
 	lea rdx, Less_Response
 	call PrintString
-	jmp exit
+	jmp exit # This is not necessary since the next statement is at the exit label.
 
 exit:
 	call EndProgram
--- week3/greater_than_zero.asm
+++ week3/greater_than_zero.asm
@@ -2,7 +2,7 @@
 
 .data
 Prompt:
-	.ascii "Enter a number: \0"
+	.ascii "Enter a number: \0" # Note that there is no "\n".  This is so that the prompt and number will be on the same line in the console.
 
 EqualToResponse:
 	.ascii "The input is equal to zero\n\0"
@@ -14,18 +14,18 @@ LessThanResponse:
 	.ascii "The input is less than zero\n\0"
 
 .text
-.global Main
+.global Main # Program Entry Point.
 
 Main:
 	lea rdx, Prompt
 	call PrintString
-	call ScanInteger
+	call ScanInteger # Returns the integer in the rdx register.
 	cmp rdx, 0
 
-	je EqualTo
+	je EqualTo # If rdx is equal to zero, then jump to the EqualTo label.
+	jg GreaterThan # If rdx is greater than zero, then jump to the GreaterThan label.
 
-	jg GreaterThan
-
+# rdx is not equal to or greater than zero, so it must be less than.
 	lea rdx, LessThanResponse
 	call PrintString
 	jmp exit
